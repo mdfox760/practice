326f1579aa5a9ab118b9164e0995b34b71cd255c
Matt Fox <matt@ROBERTs-iMac-3.router>
2017-10-03 08:32:52 -0700

    Changed editor to nano

--- easy_hash.rb
+++ easy_hash.rb
@@ -1,2 +1,11 @@
+# A readable hash.
 h = { "a" => 100, "b" => 200 }
 h.each { |key, value| puts "#{key} is #{value}" }
+
+# Another way to make a hash.
+hash = {}
+hash["Son"] = "Kai Kai"
+hash["Father"] = "Bob"
+hash.each do |type, name|
+  puts type + ': ' + name
+end
