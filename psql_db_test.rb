83c0410024ab83bdba78c184f8f8391bc80f6d09
Matt Fox <mdfox760@gmail.com>
2017-12-24 12:50:36 -0800

    edited psql_db_test.rb

--- psql_db_test.rb
+++ psql_db_test.rb
@@ -1,14 +1,11 @@
 require 'pg'
 require 'dbi'
+require 'table_print'
 
 # conn = PG.connect(hostname, username, password, databasename)
 conn = PG.connect( dbname: 'stocks')
-conn.exec( "SELECT * FROM portfolio" ) do |result|
-  puts " Name            | \tSymbol           | \tSector         | \tIndustry "
-  result.each do |row|
-    puts "    %s \t|     \t%s \t|      \t%s \t|      \t%s " %
-      row.values_at('name', 'symbol', 'sector', 'industry')
-  end
 
-end
+stock_list = conn.exec( "SELECT * FROM portfolio order by name;" )
+tp stock_list, "name", "symbol", "sector", "industry"
+
 conn.close
