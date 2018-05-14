ba06484c5d3954f3f73cea6d89ebd5fec3a95045
Matt Fox <mdfox760@gmail.com>
2018-05-14 11:50:51 -0700

    edited vararg.rb

--- vararg.rb
+++ vararg.rb
@@ -1,7 +1,7 @@
 # variable number of parameters example
 # The asterisk is actually taking all arguments you send to the method
 # and assigning them to an array named my_string as shown below
-# The do end is a Ruby block which we talk in length later
+# The do end is a Ruby block.
 def foo(*my_string)
   my_string.inspect
 end
