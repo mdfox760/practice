c33f4be0a22108a2a7acd372550ac083b9a3c413
Matt Fox <mdfox760@gmail.com>
2018-05-23 09:54:42 -0700

    Added namespace.rb

---
+++ namespace.rb
@@ -0,0 +1,14 @@
+module Perimeter
+  class Array
+    def initialize
+      @size = 400
+    end
+  end
+end
+
+# Sets Perimeter module in Array only
+our_array = Perimeter::Array.new
+ruby_array = Array.new
+
+p our_array.class
+p ruby_array.class
