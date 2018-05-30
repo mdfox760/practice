802834e3a7d026d6e24b57295f6164e465583bab
Matt Fox <mdfox760@gmail.com>
2018-05-29 21:34:40 -0700

    Edited classBank.rb

--- classBank.rb
+++ classBank.rb
@@ -1,3 +1,5 @@
+# Need to get cust_accounts to autoincrement.
+# Have not implemented Account class.
 class Bank
   attr_accessor :number, :name, :balance
 
@@ -5,7 +7,7 @@ class Bank
     number = 0
     @number = number += 1
     @name = "FoxBank"
-    @balance = "50,000,000"
+    @balance = 50000000
   end
 
   def bankRoutingNumber()
@@ -40,64 +42,94 @@ class Bank
   end
 end
 
-class NewBankAccount < Bank
-  attr_accessor :account, :name, :address, :phone, :balance
+class Account
 
-  def initialize(account, name, address, phone, balance)
+  attr_accessor :name, :balance
 
-    @account = account 
-    @address = address
+  class << self
+
+    def create_for name
+      @accounts ||= []
+      @accounts << Account.new(name)
+    end
+
+    def find_for name
+      @accounts.find { |account| account.name == name }
+    end
+  end
+
+  def initialize name
+    @balance = 0
     @name = name
-    @phone = phone
-    @balance = balance
+  end
+
+  def deposit amount
+    @balance += amount
+  end
+
+  def withdraw amount
+    @balance -= amount
+  end
+end
+
+class NewBankAccount < Bank
+  attr_accessor :cust_account, :cust_name, :cust_address, :cust_phone, :cust_balance
+
+  def initialize(cust_account, cust_name, cust_address, cust_phone, cust_balance)
+
+    @cust_account = cust_account
+    @cust_address = cust_address
+    @cust_name = cust_name
+    @cust_phone = cust_phone
+    @cust_balance = cust_balance
   end
 
   def initial_deposit
     @initial_deposit
   end
 
-  def set_account(account)
-    @account += 1
+  def set_cust_account(cust_account)
+    @cust_account += 1
   end
 
-  def get_account
-    @account
+  def get_cust_account
+    @cust_account
   end
 
-  def set_customer_name(name)
-    @name = name
+  def set_cust_name(cust_name)
+    @cust_name = cust_name
   end
 
-  def get_customer_name
-    @name
+  def get_cust_name
+    @cust_name
   end
 
-  def set_customer_address(address)
-    @address = address
+  def set_cust_address(cust_address)
+    @cust_address = cust_address
   end
 
-  def get_customer_address
-    @address
+  def get_cust_address
+    @cust_address
   end
 
-  def set_customer_phone(phone)
-    @phone = phone
+  def set_cust_phone(cust_phone)
+    @cust_phone = cust_phone
   end
 
-  def get_customer_phone
-    @phone
+  def get_cust_phone
+    @cust_phone
   end
 
-  def set_opening_balance(balance)
-    @balance = balance
+  def set_opening_balance(cust_balance)
+    @cust_balance = cust_balance
   end
 
-  def get_account_balance
-    "$" + @balance.to_s
+  def get_cust_account_balance
+    "$" + @cust_balance.to_s
   end
 
   def test_new_account
-    puts "New account opening! #{account}"
+    puts "New account opening! #{get_cust_account}"
   end
 end
 
@@ -106,10 +138,28 @@ nba = NewBankAccount.new(1, "Matt", "123 Main", "123-555-5555", 250000)
 nba2 = NewBankAccount.new(2, "Kyle", "124 Main", "456-555-5556", 50000)
 
 puts nba.test_new_account
-puts nba.get_customer_name
-puts nba.account
-puts nba.get_account_balance.to_s
+puts nba.get_cust_name
+puts nba.get_cust_account
+puts nba.get_cust_account_balance.to_s
 puts "****"*3
-puts nba2.get_customer_name
-puts nba2.account
-puts nba2.get_account_balance
+puts nba2.get_cust_name
+puts nba2.get_cust_account
+puts nba2.get_cust_account_balance
+puts "****"*3
+Account.create_for "Bob"
+Account.find_for "Bob"
+Account.create_for "Kyle"
+Account.find_for "Kyle"
+
+account = Account.find_for "Bob"
+account2 = Account.find_for "Kyle"
+account.balance = 50000
+account2.balance = 200
+
+puts "#{account.name} Deposit: $" + account.deposit(200).to_s
+puts "#{account.name} Withdraw: $" + account.withdraw(50).to_s
+puts "#{account.name} Balance: $" + account.balance.to_s
+
+puts "#{account2.name} Deposit: $" + account2.deposit(200).to_s
+puts "#{account2.name} Withdraw: $" + account2.withdraw(50).to_s
+puts "#{account2.name} Balance: $" + account2.balance.to_s
